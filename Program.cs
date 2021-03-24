89e1e26089edc53d1205c5707f89244d1e51b3a9
Sofia Rokkones <sofia.rokkones@forefront.se>
2021-03-24 14:58:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
