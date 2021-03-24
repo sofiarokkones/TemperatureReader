470ea4ca5ce6a8228198c5cfc5ceeeeef3bfdc3a
Sofia Rokkones <sofia.rokkones@forefront.se>
2021-03-24 15:22:46 +0100

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TempReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello the World!");
         }
     }
 }
